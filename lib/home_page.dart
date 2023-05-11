71a09e6320b8710aeff720a87e2be76de5a6ee76
lzlocki <lzlocki@poczta.fm>
2023-05-11 21:46:13 +0200

    Remove parts of code to be able to use SetState method. Working version

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -21,24 +23,41 @@ class _HomePageState extends State<HomePage> {
         ),
       body: Column(
         children: [
-          const TagForm(),
+          TagForm(),
+          SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: Search,
+              child: Text('Search')),
         ],
       )
       );
   }
 
-}
-
+  // list with links to pics from uri
+  List<dynamic> linksToPics = [];
 
 // https://api.flickr.com/services/feeds/photos_public.gne?tags=${this.tags}&format=json&nojsoncallback=1
-String URL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=face&format=json&nojsoncallback=1";
+  String URL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=face&format=json&nojsoncallback=1";
 // search pictures
-void Search() {
-// ToDO: code searching flickr resources here
+  void Search() async {
+    print('fetching data from flickr');
+    const URL = 'https://api.flickr.com/services/feeds/photos_public.gne?tags=face&format=json&nojsoncallback=1';
+    final uri = Uri.parse(URL);
+    // ToDO: code searching flickr resources here
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      linksToPics = json['items'];
+    });
+    print('fetching completed');
+  }
+
 }
 
+
+
 class TagForm extends StatelessWidget {
-  const TagForm({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -54,12 +73,17 @@ class TagForm extends StatelessWidget {
               )
             ),
           ),
-      SizedBox(height: 20),
-      ElevatedButton(
-          onPressed: Search,
-          child: Text('search'))
+     // ElevatedButton(
+     //     onPressed: Search,
+     //     child: Text('search')),
+         // ListView.builder(
+             // itemCount: linksToPics.length,
+              //itemBuilder: (context, index) {
+               // final item = linksToPics[index];
+                //final link = item['link']
+           // return ListTile();
+          //}),
       ],
     );
   }
-
 }
