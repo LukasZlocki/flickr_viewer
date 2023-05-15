ef867f4964c15b70b421fb7d436474feb097c59b
lzlocki <lzlocki@poczta.fm>
2023-05-15 17:11:51 +0200

    Remove unused imports

--- lib/thumbnail_screen.dart
+++ lib/thumbnail_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
 
 class ThumbnailScreen extends StatefulWidget {
   const ThumbnailScreen(
