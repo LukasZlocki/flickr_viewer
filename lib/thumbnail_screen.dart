1749967ea262830cc90344ddbbcd99281cb8191a
lzlocki <lzlocki@poczta.fm>
2023-05-17 11:53:37 +0200

    Add all working code from test screen to thumbnailscreen

--- lib/thumbnail_screen.dart
+++ lib/thumbnail_screen.dart
@@ -1,100 +1,76 @@
+import 'package:flickr_viewer/image_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-class ThumbnailScreen extends StatefulWidget {
-  const ThumbnailScreen(
-      {super.key, required this.tag, required this.urlImgList});
+class ThumbnailScreen extends StatelessWidget {
+  const ThumbnailScreen({super.key, required this.tag, required this.urlImgList});
 
   final String tag;
-  //final List<dynamic> linksToPics;
   final List<String> urlImgList;
 
-  @override
-  State<ThumbnailScreen> createState() => _ThumbnailScreenState();
-}
-
-class _ThumbnailScreenState extends State<ThumbnailScreen> {
+  final String title = "testing screen";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Thumbnails result for: ${widget.tag}"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(40.0),
-            child: GridView.builder(
-                itemCount: widget.urlImgList.length, //widget.urlImgList.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1,
-                    crossAxisSpacing: 10,
-                    childAspectRatio: (1.0),
-                    mainAxisExtent: 10
-                ), itemBuilder: (BuildContext contex, int index) {
-                  return Container(
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.0),
-                        image: DecorationImage(
-                            image: NetworkImage(widget.urlImgList[index]),
-                            )),
-                  );
-                })
+    return MaterialApp(
+      title: "Test screen",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child:
+              ImageButton(imgs: urlImgList),
+            ),
+            ElevatedButton(
+                child: Text("Back main page"),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+            //ImageButton(imgs: urlImgList),
+          ],
+        ),
       ),
     );
   }
 }
 
-// ************************************************
+class ImageButton extends StatelessWidget {
+  const ImageButton({super.key, required this.imgs});
 
-            /*
-            ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: widget.urlImgList.length,
-              itemBuilder: (context, index) {
-                final link = widget.urlImgList[index];
-                //final link2 = link['link'];
-                return ListTile(
-                  title: Text(link),
-                );
-              },
-            ),
-            */
+  final List<String> imgs;
 
-//// pozostalosci
-/*
-class _ThumbnailScreenState extends State<ThumbnailScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "thumbnails",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Thumbnails result for: ${widget.tag}"),
-        ),
-        body: Column(
-          children: [
-            ElevatedButton(
-                child: Text("<- Back main page"),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-            /*
-            ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: widget.urlImgList.length,
-              itemBuilder: (context, index) {
-                final link = widget.urlImgList[index];
-                //final link2 = link['link'];
-                return ListTile(
-                  title: Text(link),
-                );
-              },
-            ),
-            */
-            //Image.network(widget.urlImgList[5]),
-            GridView.builder(
- */
+    return GridView.count(
+      crossAxisCount: 1,
+      children: List.generate(
+          imgs.length,
+              (index) =>
+              Center(
+                  child: GridTile(
+                    child: TextButton(
+                      onPressed: () {
+                        String urlBigImg = ConvertLinkToBigImgUrl(imgs[index]);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) =>
+                                    ImageScreen(urlImg: urlBigImg)
+                                )));
+                      },
+                      child: Image.network(imgs[index]),
+                    ),
+                  ))),
+    );
+  }
+
+  // Converting url string with small img "_m" to url with big img "_b"
+  String ConvertLinkToBigImgUrl(String url) {
+    String _urlBigImg;
+    _urlBigImg = url.replaceAll("_m", "_b");
+    return _urlBigImg;
+  }
+}
